f73048ba38a52ce1fe7f5dec9d194f700b0c5639
SiHaas <simone.haas.sh@googlemail.com>
2020-05-28 22:50:12 +0200

    Touch example build
    input.touch & build options

---
+++ Assets/Scripts/TouchExample.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchExample : MonoBehaviour
+
+{
+    GameObject particle;
+
+    void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                Debug.Log("touched");
+                // Construct a ray from the current touch coordinates
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                if (Physics.Raycast(ray))
+                {
+                    // Create a particle if hit
+                    Instantiate(particle, transform.position, transform.rotation);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TouchExample2.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class TouchExample2 : MonoBehaviour
+{
+    public GameObject testCube;
+  
+    // Prints number of fingers touching the screen
+    void Update()
+    {
+        var cubeRenderer = testCube.GetComponent<Renderer>();
+        var fingerCount = 0;
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+            {
+                fingerCount++;
+            }
+        }
+        if (fingerCount > 0)
+        {
+            cubeRenderer.material.SetColor("_Color", Color.red);
+            print("User has " + fingerCount + " finger(s) touching the screen");
+        }
+    }
+}
