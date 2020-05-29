af33502960c3cae014abceded33167f18325c5cd
SiHaas <simone.haas.sh@googlemail.com>
2020-05-29 14:54:30 +0200

    Added TouchScript and tested components

---
+++ Assets/Scripts/SceneLogic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class SceneLogic : MonoBehaviour
+{
+    public static UnityAction StartGameAction { get; set; }
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        StartGameAction += StartGameFunction;
+      
+        SceneLogic.StartGameAction.Invoke();
+
+ 
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void StartGameFunction()
+    {
+        Debug.Log("StartGameAction received");
+    }
+
+}
