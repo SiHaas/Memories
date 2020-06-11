20b9873792682bc4bf030b8cd9976f241bdc7f7d
SiHaas <simone.haas.sh@googlemail.com>
2020-06-11 21:06:00 +0200

    Played around with movement

---
+++ Assets/Scripts/CameraDrive.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDrive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the object forward along its z axis 1 unit/second.
+        transform.Translate(- Vector3.forward * Time.deltaTime);
+
+        // Move the object upward in world space 1 unit/second.
+        //transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+    }
+}
