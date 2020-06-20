0a7aa384b1c05edb44c368b27b361c1c5c3ea777
SiHaas <simone.haas.sh@googlemail.com>
2020-06-20 04:19:32 +0200

    Fog particles added

--- Assets/Scripts/CameraDrive.cs
+++ Assets/Scripts/CameraDrive.cs
@@ -2,21 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraDrive : MonoBehaviour
+// A cube can be moved around the world. It is kept inside a 1 unit by 1 unit
+// xz space. A small, long cylinder is created and positioned away from the center of
+// the 1x1 unit. The cylinder is moved between two locations. Each time the cylinder is
+// positioned the cube moves towards it. When the cube reaches the cylinder the cylinder
+// is re-positioned to the other location. The cube then changes direction and moves
+// towards the cylinder again.
+//
+// A floor object is created for you.
+//
+// To view this example, create a new 3d Project and create a Cube placed at
+// the origin. Create Example.cs and change the script code to that shown below.
+// Save the script and add to the Cube.
+//
+// Now run the example.
+
+public class CameraDrive : MonoBehaviour { 
+// Adjust the speed for the application.
+    public float speed = 1.0f;
+
+// The target (cylinder) position.
+private Transform target;
+
+void Awake()
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Position the cube at the origin.
+    transform.position = new Vector3(0.0f, 0.0f, 0.0f);
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Move the object forward along its z axis 1 unit/second.
-        transform.Translate(- Vector3.forward * Time.deltaTime);
+    // Create and position the cylinder. Reduce the size.
+    GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+    Camera.main.transform.position = new Vector3(0.85f, 1.0f, -3.0f);
 
-        // Move the object upward in world space 1 unit/second.
-        //transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+    // Grab cylinder values and place on the target.
+    target = cylinder.transform;
+    target.transform.localScale = new Vector3(0.15f, 1.0f, 0.15f);
+    target.transform.position = new Vector3(0.8f, 0.0f, 0.8f);
+
+    // Position the camera.
+    Camera.main.transform.position = new Vector3(0.85f, 1.0f, -3.0f);
+    Camera.main.transform.localEulerAngles = new Vector3(15.0f, -20.0f, -0.5f);
+
+    // Create and position the floor.
+    GameObject floor = GameObject.CreatePrimitive(PrimitiveType.Plane);
+    floor.transform.position = new Vector3(0.0f, -1.0f, 0.0f);
+}
+
+void Update()
+{
+    // Move our position a step closer to the target.
+    float step = speed * Time.deltaTime; // calculate distance to move
+    transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+    // Check if the position of the cube and sphere are approximately equal.
+    if (Vector3.Distance(transform.position, target.position) < 0.001f)
+    {
+        // Swap the position of the cylinder.
+        target.position *= -1.0f;
     }
+}
 }
