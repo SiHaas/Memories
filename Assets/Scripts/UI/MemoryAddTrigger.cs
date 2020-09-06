021efdec4b58ad4ac1fe738cf5507cbd1ce20506
SiHaas <simone.haas.sh@googlemail.com>
2020-09-06 17:39:26 +0200

    Backup

--- Assets/Scripts/UI/MemoryAddTrigger.cs
+++ Assets/Scripts/UI/MemoryAddTrigger.cs
@@ -1,12 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TouchScript.Gestures;
 
 public class MemoryAddTrigger : MonoBehaviour
 {
+    [SerializeField]
+    private TapGesture Touched;
+
+    private void OnEnable()
+    {
+        //TwoFingerMoveGesture.Transformed += twoFingerTransformHandler;
+        Touched = GetComponent<TapGesture>();
+        Touched.Tapped += touched;
+        Debug.Log("miau");
+    }
     private void OnMouseDown()
     {
         Debug.Log("Test");
         UiManager.AddMemoryItemAction.Invoke();
     }
+    private void touched(object sender, System.EventArgs e)
+    {
+        UiManager.AddMemoryItemAction.Invoke();
+    }
 }
+
